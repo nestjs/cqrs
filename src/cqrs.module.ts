bb0c3ea3358ec2d0f39e8ba9f4473ff76461029e
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-03-27 20:22:50 +0200

    format: add missing lint-staged extension

--- src/cqrs.module.ts
+++ src/cqrs.module.ts
@@ -1,18 +1,10 @@
-import { Module } from '@nestjs/common';
-import { CommandBus } from './command-bus';
-import { EventPublisher } from './event-publisher';
-import { EventBus } from './event-bus';
+import { Module } from "@nestjs/common";
+import { CommandBus } from "./command-bus";
+import { EventPublisher } from "./event-publisher";
+import { EventBus } from "./event-bus";
 
 @Module({
-    components: [
-        CommandBus,
-        EventBus,
-        EventPublisher,
-    ],
-    exports: [
-        CommandBus,
-        EventBus,
-        EventPublisher,
-    ]
+  providers: [CommandBus, EventBus, EventPublisher],
+  exports: [CommandBus, EventBus, EventPublisher]
 })
 export class CQRSModule {}
