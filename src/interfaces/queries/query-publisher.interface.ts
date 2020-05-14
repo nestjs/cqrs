1b13961f920a54b09f7acb6ada4afdd89ffffe29
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2020-05-14 09:46:17 +0200

    fix(): fix invalid import (query interface)

--- src/interfaces/queries/query-publisher.interface.ts
+++ src/interfaces/queries/query-publisher.interface.ts
@@ -1,4 +1,4 @@
-import { IQuery } from './Query.interface';
+import { IQuery } from './query.interface';
 
 export interface IQueryPublisher<QueryBase extends IQuery = IQuery> {
   publish<T extends QueryBase = QueryBase>(query: T): any;
