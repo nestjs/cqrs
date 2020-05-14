1a06e4cd8abc8e469e31e2b7dd17f8d86786c4f4
Rodrigo <monstawoodwow@gmail.com>
2020-05-14 13:36:32 +0100

    Decorator docs

--- src/decorators/command-handler.decorator.ts
+++ src/decorators/command-handler.decorator.ts
@@ -2,6 +2,11 @@ import 'reflect-metadata';
 import { ICommand } from '../index';
 import { COMMAND_HANDLER_METADATA } from './constants';
 
+/**
+ * CommandHandler handles actions dispatched on `CommandBus`
+ * @param command `ICommand`
+ * @see https://docs.nestjs.com/recipes/cqrs#commands
+ */
 export const CommandHandler = (command: ICommand): ClassDecorator => {
   return (target: object) => {
     Reflect.defineMetadata(COMMAND_HANDLER_METADATA, command, target);
