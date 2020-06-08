49555228cdddbfa04380967e7b6504916c124341
Sam Goldmann <sam.goldmann@gmail.com>
2020-06-08 10:29:48 -0400

    fix: Change AggregateRoot's commit() to use publishAll()
    Using publishAll() instead of publish() enables implementations of IEventBus to
    support correct transactional semantics when used together with EventPublisher.

--- src/event-publisher.ts
+++ src/event-publisher.ts
@@ -16,6 +16,10 @@ export class EventPublisher<EventBase extends IEvent = IEvent> {
   ): T {
     const eventBus = this.eventBus;
     return class extends metatype {
+      publish(event: EventBase) {
+        eventBus.publish(event);
+      }
+
       publishAll(events: EventBase[]) {
         eventBus.publishAll(events);
       }
@@ -24,6 +28,10 @@ export class EventPublisher<EventBase extends IEvent = IEvent> {
 
   mergeObjectContext<T extends AggregateRoot<EventBase>>(object: T): T {
     const eventBus = this.eventBus;
+    object.publish = (event: EventBase) => {
+      eventBus.publish(event);
+    };
+
     object.publishAll = (events: EventBase[]) => {
       eventBus.publishAll(events);
     };
