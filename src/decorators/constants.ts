fa3af7049aa7c78ae9a5d5bfd531728ce8ac0578
Robert Lowe <robert@iblargz.com>
2021-09-22 10:04:27 -0400

    Fix typo
    Fix trivial typo

--- src/decorators/constants.ts
+++ src/decorators/constants.ts
@@ -1,4 +1,4 @@
 export const COMMAND_HANDLER_METADATA = '__commandHandler__';
 export const QUERY_HANDLER_METADATA = '__queryHandler__';
-export const EVENTS_HANDLER_METADATA = '__eventsHandler';
+export const EVENTS_HANDLER_METADATA = '__eventsHandler__';
 export const SAGA_METADATA = '__saga__';
