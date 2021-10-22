b908fe4324b9adcd5b9c98fac83cd5c46ef32cb7
Maciej Sikorski <maciej.sikorski@valueadd.pl>
2021-10-22 09:58:27 +0200

    fix(): don't resolve handlers based on names
    Previously, the handler for command/query (action) was assigned by the
    action name. When there were multiple actions with the same name
    handlers were overwritten.
    Use ids to differentiate actions with the same name.
    
    Closes @nestjs/cqrs#787

--- src/command-bus.ts
+++ src/command-bus.ts
@@ -1,7 +1,7 @@
 import { Injectable, Type } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import 'reflect-metadata';
-import { COMMAND_HANDLER_METADATA } from './decorators/constants';
+import { COMMAND_HANDLER_METADATA, COMMAND_METADATA } from './decorators/constants';
 import { CommandHandlerNotFoundException } from './exceptions/command-not-found.exception';
 import { DefaultCommandPubSub } from './helpers/default-command-pubsub';
 import { InvalidCommandHandlerException } from './index';
@@ -12,6 +12,7 @@ import {
   ICommandPublisher,
 } from './interfaces/index';
 import { ObservableBus } from './utils/observable-bus';
+import { CommandMetadata } from './interfaces/commands/command-metadata.interface';
 
 export type CommandHandlerType = Type<ICommandHandler<ICommand>>;
 
@@ -36,17 +37,17 @@ export class CommandBus<CommandBase extends ICommand = ICommand>
   }
 
   execute<T extends CommandBase, R = any>(command: T): Promise<R> {
-    const commandName = this.getCommandName(command as any);
-    const handler = this.handlers.get(commandName);
+    const commandId = this.getCommandId(command);
+    const handler = this.handlers.get(commandId);
     if (!handler) {
-      throw new CommandHandlerNotFoundException(commandName);
+      throw new CommandHandlerNotFoundException(commandId);
     }
     this.subject$.next(command);
     return handler.execute(command);
   }
 
-  bind<T extends CommandBase>(handler: ICommandHandler<T>, name: string) {
-    this.handlers.set(name, handler);
+  bind<T extends CommandBase>(handler: ICommandHandler<T>, id: string) {
+    this.handlers.set(id, handler);
   }
 
   register(handlers: CommandHandlerType[] = []) {
@@ -58,20 +59,36 @@ export class CommandBus<CommandBase extends ICommand = ICommand>
     if (!instance) {
       return;
     }
-    const target = this.reflectCommandName(handler);
+    const target = this.reflectCommandId(handler);
     if (!target) {
       throw new InvalidCommandHandlerException();
     }
-    this.bind(instance as ICommandHandler<CommandBase>, target.name);
+    this.bind(instance as ICommandHandler<CommandBase>, target);
   }
 
-  private getCommandName(command: Function): string {
-    const { constructor } = Object.getPrototypeOf(command);
-    return constructor.name as string;
+  private getCommandId(command: CommandBase): string {
+    const { constructor: commandType } = Object.getPrototypeOf(command);
+    const commandMetadata: CommandMetadata = Reflect.getMetadata(
+      COMMAND_METADATA,
+      commandType,
+    );
+    if (!commandMetadata) {
+      throw new CommandHandlerNotFoundException(commandType.name);
+    }
+
+    return commandMetadata.id;
   }
 
-  private reflectCommandName(handler: CommandHandlerType): FunctionConstructor {
-    return Reflect.getMetadata(COMMAND_HANDLER_METADATA, handler);
+  private reflectCommandId(handler: CommandHandlerType): string | undefined {
+    const command: Type<ICommand> = Reflect.getMetadata(
+      COMMAND_HANDLER_METADATA,
+      handler,
+    );
+    const commandMetadata: CommandMetadata = Reflect.getMetadata(
+      COMMAND_METADATA,
+      command,
+    );
+    return commandMetadata.id;
   }
 
   private useDefaultPublisher() {
