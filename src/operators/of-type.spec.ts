85c5d7e4fb927afbdea1df50a8ba59a1e7ce2e3f
wSedlacek <wsedlacekc@gmail.com>
2021-11-30 22:51:09 -0800

    fix: type inference of type

--- src/operators/of-type.spec.ts
+++ src/operators/of-type.spec.ts
@@ -3,10 +3,17 @@ import { ofType } from './of-type';
 import { IEvent } from '../interfaces';
 
 describe('operators/ofType', () => {
-  class A implements IEvent {}
-  class B implements IEvent {}
+  class A implements IEvent {
+    keyAOrB: string = 'keyAOrB';
+    keyAOnly: string = 'keyAOnly';
+  }
+  class B implements IEvent {
+    keyAOrB: string = 'keyAOrB';
+  }
   class SubA extends A {}
-  class C implements IEvent {}
+  class C implements IEvent {
+    keyCOptional?: string;
+  }
 
   let stream: Subject<any>;
   let output: IEvent[];
@@ -32,7 +39,7 @@ describe('operators/ofType', () => {
     expectedResults.push(new A());
 
     stream.next(new B());
-    stream.next(...expectedResults);
+    expectedResults.forEach((event) => stream.next(event));
     stream.next(new Date());
 
     expect(output).toEqual(expectedResults);
@@ -45,5 +52,36 @@ describe('operators/ofType', () => {
     expectedResults.forEach((event) => stream.next(event));
 
     expect(output).toEqual(expectedResults);
+  });
+
+  // TypeScript TSC test, if this compiles it passes
+  it('should infer return types of similar unions unions', (done) => {
+    stream.pipe(ofType(A, B)).subscribe((event) => {
+      event.keyAOrB;
+      // @ts-expect-error -- A | B cannot reference a B only key
+      event.keyBOnly;
+      done();
+    });
+
+    stream.next(new A());
+  });
+
+  // TypeScript TSC test, if this compiles it passes
+  it('should infer return types of disparate unions', (done) => {
+    stream.pipe(ofType(A, C)).subscribe((event) => {
+      // @ts-expect-error -- A | C cannot reference a key that is not on C
+      event.keyAOnly;
+
+      if (event instanceof A) {
+        event.keyAOnly;
+      }
+
+      if (event instanceof C) {
+        event.keyCOptional;
+      }
+      done();
+    });
+
+    stream.next(new A());
   });
 });
