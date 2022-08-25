cb9de12cb20c25c5b96f97bbd56c77020e525e66
Ricardo Baptista <ricardogermano85@gmail.com>
2022-08-25 18:49:19 -0300

    fix: command handler decorator argument type

--- src/decorators/command-handler.decorator.ts
+++ src/decorators/command-handler.decorator.ts
@@ -13,7 +13,9 @@ import { v4 } from 'uuid';
  *
  * @see https://docs.nestjs.com/recipes/cqrs#commands
  */
-export const CommandHandler = (command: ICommand): ClassDecorator => {
+export const CommandHandler = (
+  command: new (...args: unknown[]) => ICommand,
+): ClassDecorator => {
   return (target: object) => {
     if (!Reflect.hasMetadata(COMMAND_METADATA, command)) {
       Reflect.defineMetadata(COMMAND_METADATA, { id: v4() }, command);
